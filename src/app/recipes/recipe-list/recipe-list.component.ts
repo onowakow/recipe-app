69cc350e010168277e44315e3d89147a698babe3
onowakow <nowakowskiowen08@gmail.com>
2023-01-25 08:47:29 -0700

    Finish building static web app.

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent {
+  recipes: Recipe[] = [
+    new Recipe(
+      'Soft-boiled Eggs',
+      'Eggs boiled in shell with melted yolk',
+      'https://www.cookipedia.co.uk/wiki/images/2/24/5_minute_hard_boiled_egg.jpg'
+    ),
+    new Recipe(
+      'Polenta',
+      'A corn porridge',
+      'https://live.staticflickr.com/65535/50643182893_766dfe97ac_b.jpg'
+    ),
+  ];
 
+  constructor() {}
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,7 +1,15 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
-  templateUrl: 'shopping-list.component.html'
+  templateUrl: 'shopping-list.component.html',
 })
-export class ShoppingListComponent {}
+export class ShoppingListComponent {
+  ingredients: Ingredient[] = [
+    new Ingredient('Eggs', 12),
+    new Ingredient('Tomatoes', 10),
+  ];
+
+  constructor() {}
+}
